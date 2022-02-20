0cd502c85217a39210085dedc9a643e6dbcdadce
gani-ablay <gani.ablay@gmail.com>
2022-02-21 01:05:15 +0300

    example with profiles:logger, console, prod

--- src/main/java/ru/learnup/vtb/library/libraryapplication/config/LoggersConfig.java
+++ src/main/java/ru/learnup/vtb/library/libraryapplication/config/LoggersConfig.java
@@ -10,18 +10,18 @@ import ru.learnup.vtb.library.libraryapplication.services.EmailLogger;
 import ru.learnup.vtb.library.libraryapplication.services.interfaces.Logger;
 
 @Configuration
-//@Profile("!logger")
+@Profile("logger")
 public class LoggersConfig {
 
     @Bean
     @Scope("prototype")
-    @Profile("!console")
+    @Profile("console")
     public Logger consoleLogger(@Value("${myConfig.logger.console.prefix:default}")String prefix){
         return new ConsoleLogger(prefix);
     }
 
     @Bean
-    @Profile("console")
+    @Profile("!console")
     public Logger emailLogger(){
         return new EmailLogger();
     }
